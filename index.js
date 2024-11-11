c1e8ecd1bfbb07d3bed555b3fa40d8c6ef3a6088
“Abdul <kaazees31@gmail.com>
2024-11-11 20:00:06 +0530

    new

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import * as dotenv from "dotenv";
 import db from "./config/db.js";
 import  create  from "./routes/createRoute.js";
 import UserRouter from "./routes/userRoute.js";
+import cors from "cors"
+const allowedOrigins = ['http://localhost:3000', 'https://blog-app-backend-3vsi.onrender.com'];
 
 dotenv.config();
 
@@ -13,6 +15,20 @@ db()
 
 // Middleware to parse JSON requests
 app.use(express.json());
+
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP methods
+    credentials: true // Allow cookies if needed
+  }));
+  
+app.use(express.urlencoded({ extended: false }));
 
 // Port configuration with a fallback to port 3000
 const PORT = process.env.PORT;
